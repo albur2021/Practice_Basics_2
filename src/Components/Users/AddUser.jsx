5e170addd40ad016ed825774f0d7b40b666e54ac
Al Bur <albur2021@yahoo.com>
2021-08-25 12:33:58 -0400

    set 'values args on input name and age to empty them after user has submitted

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -18,15 +18,17 @@ const ageChangeHandler = (event) =>{
 const addUserHandler = (event)=>{
     event.preventDefault();
     console.log(enteredUserName, enteredAge);
+    setEnteredUserName('');
+    setEnteredAge('');
 };
 
 return(
   <Card className={classes.input}>
        <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler}/>
+        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUserName}/>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge}/>
         <AddButton type="submit">Add User</AddButton>
     </form>
   </Card>
