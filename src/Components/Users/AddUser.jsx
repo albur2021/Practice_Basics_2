3509a76ca61a536e809bb378df5b3c2860eb82cc
Al Bur <albur2021@yahoo.com>
2021-08-25 12:47:28 -0400

    validate whether the user input like name and age are not empty and age is number

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -17,7 +17,19 @@ const ageChangeHandler = (event) =>{
 
 const addUserHandler = (event)=>{
     event.preventDefault();
+    
+
+    //Validation - is not empty, 
+    //'trim()' removes all white space, 
+    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+        return;
+    }
+    //and '+enteredAge' helps to identify whether it's number
+    if(+enteredAge < 1){
+        return;
+    }
     console.log(enteredUserName, enteredAge);
+
     setEnteredUserName('');
     setEnteredAge('');
 };
