9b198f1df8c180db42d0212a67656b87a4354d96
Al Bur <albur2021@yahoo.com>
2021-08-25 12:02:42 -0400

    add UI/Card.module.css and Users/AddUser.module.css and Style Card Comp by implementing internal and external css files

--- src/Components/UI/Card.jsx
+++ src/Components/UI/Card.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import classes from "./Card.module.css";
 
-
-const Card = (props) =>{
-
-return(
-  <div  className=>{props.children}>
-   
-  </div>
+const Card = (props) => {
+  return(
+    <div className={`${classes.card} ${props.className}`}>
+        {props.children}
+    </div>
   )
-}
-
+};
 
 export default Card;
