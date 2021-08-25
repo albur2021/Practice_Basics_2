9ae7a4765353c2c1023b996e13b07ab166d06c78
Al Bur <albur2021@yahoo.com>
2021-08-25 14:15:53 -0400

    add 'UI/ErrorModule Comp and UI/ErrorModul.module.css' in respect to show errors in 'Users/AddUser'

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,53 +1,62 @@
-import React, { useState } from 'react';
-import AddButton from '../UI/AddButton';
-import Card from '../UI/Card';
-import classes from './AddUser.module.css';
+import React, { useState } from "react";
+import AddButton from "../UI/AddButton";
+import Card from "../UI/Card";
+import ErrorModul from "../UI/ErrorModul";
+import classes from "./AddUser.module.css";
 
-const AddUser = (props) =>{
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-const [enteredUserName,setEnteredUserName]=useState('');
-const [enteredAge, setEnteredAge]=useState('');
-
-const usernameChangeHandler = (event) =>{
+  const usernameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
-}
-const ageChangeHandler = (event) =>{
+  };
+  const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
-}
+  };
 
-const addUserHandler = (event)=>{
+  const addUserHandler = (event) => {
     event.preventDefault();
-    
 
-    //Validation - is not empty, 
-    //'trim()' removes all white space, 
-    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
-        return;
+    //Validation - is not empty,
+    //'trim()' removes all white space,
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
     }
     //and '+enteredAge' helps to identify whether it's number
-    if(+enteredAge < 1){
-        return;
+    if (+enteredAge < 1) {
+      return;
     }
     // console.log(enteredUserName, enteredAge);
-    props.onAddUser( enteredUserName, enteredAge);// comming from App.js
-    setEnteredUserName('');// sets to update/empty input after been submitted
-    setEnteredAge('');
+    props.onAddUser(enteredUserName, enteredAge); // comming from App.js
+    setEnteredUserName(""); // sets to update/empty input after been submitted
+    setEnteredAge("");
+  };
+
+  return (
+    <div>
+      <ErrorModul title="An error ocurred!" message="Something went wrong!" />
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangeHandler}
+            value={enteredUserName}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <AddButton type="submit">Add User</AddButton>
+        </form>
+      </Card>
+    </div>
+  );
 };
-
-return(
-  <Card className={classes.input}>
-       <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUserName}/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge}/>
-        <AddButton type="submit">Add User</AddButton>
-    </form>
-  </Card>
-   
-  
-  )
-}
-
 
 export default AddUser;
