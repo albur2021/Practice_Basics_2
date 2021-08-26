88a82c7ba45e02cc45599931c3c8b1a1a35e3f23
Al Bur <albur2021@yahoo.com>
2021-08-26 12:10:55 -0400

    Add Helpers/Wrapper Comp to help to AddUser Comp to wrap and return JSX Coding by providing 'props.children' as html element to reduce its content

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -3,11 +3,12 @@ import AddButton from "../UI/AddButton";
 import Card from "../UI/Card";
 import ErrorModul from "../UI/ErrorModul";
 import classes from "./AddUser.module.css";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  const [error, setError]=useState();
+  const [error, setError] = useState();
 
   const usernameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
@@ -16,8 +17,8 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
-  const errorHandler = () =>{
-      setError(null);//set to falsy value;
+  const errorHandler = () => {
+    setError(null); //set to falsy value;
   };
 
   const addUserHandler = (event) => {
@@ -26,18 +27,18 @@ const AddUser = (props) => {
     //Validation - is not empty,
     //'trim()' removes all white space,
     if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
-        setError({
-            title: "Invalid input",
-            message: "Please enter a valid name and age (non-empty values)."
-        });
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     //and '+enteredAge' helps to identify whether it's number
     if (+enteredAge < 1) {
-        setError({
-            title: "Invalid age",
-            message: "Please enter a valid age (>0)."
-        });
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0).",
+      });
       return;
     }
     // console.log(enteredUserName, enteredAge);
@@ -47,8 +48,14 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
-      {error && <ErrorModul title={error.title} message={error.message} onConfirm={errorHandler} />}
+    <Wrapper>
+      {error && (
+        <ErrorModul
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -68,7 +75,7 @@ const AddUser = (props) => {
           <AddButton type="submit">Add User</AddButton>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
